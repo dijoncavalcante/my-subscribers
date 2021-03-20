a551fbfcdd41343097b1908dd07a4d73a3154934
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-20 18:29:28 -0400

    Add Database class

---
+++ app/src/main/java/com/dijon/mysubscribers/data/db/AppDataBase.kt
@@ -0,0 +1,36 @@
+package com.dijon.mysubscribers.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.dijon.mysubscribers.data.db.dao.SubscriberDAO
+import com.dijon.mysubscribers.data.db.entity.SubscriberEntity
+
+@Database(entities = [SubscriberEntity::class], version = 1)
+abstract class AppDataBase : RoomDatabase() {
+
+    abstract val subscriberDAO: SubscriberDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDataBase? = null
+
+        fun getInstance(context: Context): AppDataBase {
+            synchronized(this) {
+                var instance: AppDataBase? = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context,
+                        AppDataBase::class.java,
+                        "app_database"
+                    ).build()
+                }
+
+                return instance
+            }
+        }
+    }
+
+
+}
