60eb0b7ecbf7aad02a2e97be4abc5f5203f4b658
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-20 15:28:09 -0400

    Add new layout to subscriver

---
+++ app/src/main/java/com/dijon/mysubscribers/ui/subscriber/SubscriberFragment.kt
@@ -0,0 +1,20 @@
+package com.dijon.mysubscribers.ui.subscriber
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dijon.mysubscribers.R
+
+class SubscriberFragment : Fragment() {
+
+    private lateinit var viewModel: SubscriberViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.subscriber_fragment, container, false)
+    }
+}
---
+++ app/src/main/java/com/dijon/mysubscribers/ui/subscriber/SubscriberViewModel.kt
@@ -0,0 +1,7 @@
+package com.dijon.mysubscribers.ui.subscriber
+
+import androidx.lifecycle.ViewModel
+
+class SubscriberViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
