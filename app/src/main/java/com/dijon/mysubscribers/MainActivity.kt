9da42c5072e1f90ac72010df20af289a8e87f0c4
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-20 16:51:03 -0400

    Add style for toolbar

--- app/src/main/java/com/dijon/mysubscribers/MainActivity.kt
+++ app/src/main/java/com/dijon/mysubscribers/MainActivity.kt
@@ -2,10 +2,21 @@ package com.dijon.mysubscribers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //recuperando um nav controller
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        app_toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
