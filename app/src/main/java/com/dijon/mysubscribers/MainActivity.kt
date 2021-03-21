c4c8d479c51c8314200044f8cc32239c03764484
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-21 15:41:23 -0400

    Add back icon, add code to controller this function[back]

--- app/src/main/java/com/dijon/mysubscribers/MainActivity.kt
+++ app/src/main/java/com/dijon/mysubscribers/MainActivity.kt
@@ -2,21 +2,38 @@ package com.dijon.mysubscribers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //recuperando um nav controller
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        setSupportActionBar(app_toolbar)
 
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        app_toolbar.setupWithNavController(navController, appBarConfiguration)
+        //recuperando um nav controller
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        //set icon to any fragment that is called
+        navController.addOnDestinationChangedListener { _, _, _ ->
+            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
