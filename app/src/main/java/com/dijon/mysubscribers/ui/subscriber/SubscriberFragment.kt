6e14ac10297be49f1a9e287b627f5056908dfb24
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-21 12:28:23 -0400

    Created a extension to hide keyboard, Add focus on fragment, created events to hear view

--- app/src/main/java/com/dijon/mysubscribers/ui/subscriber/SubscriberFragment.kt
+++ app/src/main/java/com/dijon/mysubscribers/ui/subscriber/SubscriberFragment.kt
@@ -1,5 +1,8 @@
 package com.dijon.mysubscribers.ui.subscriber
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
@@ -7,8 +10,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.dijon.mysubscribers.R
 import com.dijon.mysubscribers.data.db.AppDataBase
 import com.dijon.mysubscribers.data.db.dao.SubscriberDAO
+import com.dijon.mysubscribers.extension.hideKeyboard
 import com.dijon.mysubscribers.repository.DatabaseDataSource
 import com.dijon.mysubscribers.repository.SubscriberRepository
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.subscriber_fragment.*
 
 class SubscriberFragment : Fragment(R.layout.subscriber_fragment) {
 
@@ -22,6 +28,53 @@ class SubscriberFragment : Fragment(R.layout.subscriber_fragment) {
                 val repository: SubscriberRepository = DatabaseDataSource(subscriberDAO)
                 return SubscriberViewModel(repository) as T
             }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeEvents()//observar os eventos do view model e escutar os liveData
+        setListeners()//escutar os eventos de click da view e outros eventos
+    }
+
+    private fun observeEvents() {
+        viewModel.subscriberStateEventData.observe(viewLifecycleOwner) { subscriberState ->
+            when (subscriberState) {
+                is SubscriberViewModel.SubscriberState.Inserted -> {
+                    //limpar os campos
+                    clearFields()
+                    //forçar esconder teclado
+                    hideKeyboard()
+                }
+            }
+        }
+
+        viewModel.messageEventData.observe(viewLifecycleOwner) { stringResId ->
+            Snackbar.make(requireView(), stringResId, Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun clearFields() {
+        input_name.text?.clear()
+        input_email.text?.clear()
+    }
+
+    private fun hideKeyboard() {
+        val parentActivity = requireActivity()
+        if (parentActivity is AppCompatActivity) {
+            parentActivity.hideKeyboard()
+        }
+    }
+
+    //escutando o botao para criar um subscriber
+    private fun setListeners() {
+        button_subscriber.setOnClickListener {
+            val name = input_name.text.toString()
+            val email = input_email.text.toString()
+
+            viewModel.addSubscriber(name, email)
         }
     }
 }
