a34240a00334c6bbeb02e968711d8298f5826ebd
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-21 11:52:34 -0400

    Add factory implementation on view

--- app/src/main/java/com/dijon/mysubscribers/ui/subscriber/SubscriberFragment.kt
+++ app/src/main/java/com/dijon/mysubscribers/ui/subscriber/SubscriberFragment.kt
@@ -1,20 +1,27 @@
 package com.dijon.mysubscribers.ui.subscriber
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.dijon.mysubscribers.R
+import com.dijon.mysubscribers.data.db.AppDataBase
+import com.dijon.mysubscribers.data.db.dao.SubscriberDAO
+import com.dijon.mysubscribers.repository.DatabaseDataSource
+import com.dijon.mysubscribers.repository.SubscriberRepository
 
-class SubscriberFragment : Fragment() {
+class SubscriberFragment : Fragment(R.layout.subscriber_fragment) {
 
-    private lateinit var viewModel: SubscriberViewModel
+    //implementação do factory
+    private val viewModel: SubscriberViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val subscriberDAO: SubscriberDAO =
+                    AppDataBase.getInstance(requireContext()).subscriberDAO
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.subscriber_fragment, container, false)
+                val repository: SubscriberRepository = DatabaseDataSource(subscriberDAO)
+                return SubscriberViewModel(repository) as T
+            }
+        }
     }
 }
