49210b99f45f781e3221635707fe5805149aae21
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-21 14:32:42 -0400

    View model to SubscriberList alread instanciado

--- app/src/main/java/com/dijon/mysubscribers/ui/subscriberlist/SubscriberListFragment.kt
+++ app/src/main/java/com/dijon/mysubscribers/ui/subscriberlist/SubscriberListFragment.kt
@@ -3,13 +3,32 @@ package com.dijon.mysubscribers.ui.subscriberlist
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.dijon.mysubscribers.R
+import com.dijon.mysubscribers.data.db.AppDataBase
+import com.dijon.mysubscribers.data.db.dao.SubscriberDAO
 import com.dijon.mysubscribers.data.db.entity.SubscriberEntity
+import com.dijon.mysubscribers.repository.DatabaseDataSource
+import com.dijon.mysubscribers.repository.SubscriberRepository
+import com.dijon.mysubscribers.ui.subscriber.SubscriberViewModel
 import kotlinx.android.synthetic.main.subscriber_list_fragment.*
 
 class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
 
-    private lateinit var viewModel: SubscriberListViewModel
+    //implementação do factory
+    private val viewModel: SubscriberListViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val subscriberDAO: SubscriberDAO =
+                    AppDataBase.getInstance(requireContext()).subscriberDAO
+
+                val repository: SubscriberRepository = DatabaseDataSource(subscriberDAO)
+                return SubscriberListViewModel(repository) as T
+            }
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
